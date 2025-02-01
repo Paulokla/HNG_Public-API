7726637e8aaf2ebfb7e8513bac5fd3410717dd06
paulokla <paulokla654@gmail.com>
2025-02-01 12:44:25 +0100

    changed the format of my time

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 CORS(app)
 
 REGISTERED_EMAIL = "paulokla654@gmail.com"
-GITHUB_URL = "https://github.com/Paulokla/HNG_Public-API.git"
+GITHUB_URL = "https://github.com/Paulokla/HNG_Public-API"
 
 @app.route("/", methods=["GET"])
 @app.route('/api', methods=['GET'])
 def get_info():
-    current_time = datetime.now().isoformat()
+    current_time = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
     response = {
         "email": REGISTERED_EMAIL,
         "current_datetime": current_time,
